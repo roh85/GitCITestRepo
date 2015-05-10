2f643e28927f6e93d87c0d5597875e02ebf02922
unknown <robby.holsteyns@gmail.com>
2015-05-10 21:58:28 +0200

    added test print

--- GitCITestRepo/Program.cs
+++ GitCITestRepo/Program.cs
@@ -11,6 +11,7 @@ namespace GitCITestRepo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Test");
             Console.ReadLine();
         }
     }
