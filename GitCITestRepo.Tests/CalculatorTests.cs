cdc9b7aba1406512ff3a5c2dfe4ae8d7d087c46f
unknown <robby.holsteyns@gmail.com>
2015-05-12 00:00:57 +0200

    Added sln and test project

---
+++ GitCITestRepo.Tests/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GitCITestRepo.Program;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+
+namespace GitCITestRepo.Program.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            var mock = new Mock<ICalculator>();
+            mock.Setup(calculator => calculator.Multiply(2, 3)).Returns(6);
+        }
+    }
+}
