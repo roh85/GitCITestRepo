68f201fc98a34b19ab27de0ef5850684467b2f89
roh85 <robby.holsteyns@gmail.com>
2015-10-02 11:19:29 +0200

    Add resharper-clt-parser powershell script

---
+++ resharper-clt-parser.ps1
@@ -0,0 +1,28 @@
+Write-Host "*******************************************************************";
+Write-Host "***********             Resharper-CLT parser      ****************";
+Write-Host "*******************************************************************";
+if($args.Length -lt 1) {
+    Write-Host "Not enough arguments passed, skipping script";
+    return;
+}
+$file = $args[0];
+
+$xml = [xml](get-content $file)
+$îssueTypes = $xml.SelectNodes("/Report/IssueTypes/IssueType")
+$issues = $xml.SelectNodes("/Report/Issues/Project/Issue")
+$merged = @()
+
+foreach($i in $issues){
+	$issueType = $îssueTypes | ? { $_.Id -eq $i.TypeId }
+	$merged += New-Object PSObject -Property @{
+		File = $i.File
+		Message = $i.Message
+		Severity = $issueType.Severity
+		Line = $i.Line
+	}
+}
+
+foreach($el in $merged){
+		Write-Host $el
+		Add-AppveyorMessage -Message $el.Message -Category $el.Severity -Details $el.File + " " $el.Line
+}
