a5aa8695c3cd5df0345b5a35920a06e7cf8627d1
roh85 <robby.holsteyns@gmail.com>
2015-10-11 10:37:35 +0200

    Change hardcoded values to variables and added some comments

--- run-build.ps1
+++ run-build.ps1
@@ -1,13 +1,23 @@
 $PREBUILD_SCRIPT = Join-Path $PSScriptRoot "prebuild.ps1"
+$BUILD_SCRIPT = Join-Path $PSScriptRoot "build.ps1"
 
+$SOLUTION_FILE = Join-Path $PSScriptRoot "GitCITestRepo.sln"
+
+# Debug, Release
+$BUILD_CONFIGURATION = "Release"
+
+# Output directory that will contain all compiled binaries
+$ARTIFACTS_DIR = Join-Path -Path $PSScriptRoot -ChildPath ".build"
+
+# Execute prebuild script to install all prerequisites
 Invoke-Expression $PREBUILD_SCRIPT
 
+# Build script uses PSake so import the PSake module
 Import-Module $env:ChocolateyInstall\lib\psake\tools\psake.psm1
 
-$BUILD_FOLDER = Join-Path -Path $PSScriptRoot -ChildPath ".build"
-
-Invoke-Psake .\build.ps1 -properties @{
-	"build_artifacts_dir"=$BUILD_FOLDER; 
-	"configuration"="Release";
-	"solution"="GitCITestRepo.sln"
+# Execute PSake build tasks
+Invoke-Psake $BUILD_SCRIPT -properties @{
+	"build_artifacts_dir"=$ARTIFACTS_DIR; 
+	"configuration"=$BUILD_CONFIGURATION;
+	"solution"="$SOLUTION_FILE"
 }
