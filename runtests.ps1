519aab625018fd1d7b98e7375479a48db8f6e45a
roh85 <robby.holsteyns@gmail.com>
2015-10-14 17:07:07 +0200

    Add code coverage with opencover and report generator

--- runtests.ps1
+++ runtests.ps1
@@ -5,7 +5,8 @@ if($args.Length -lt 1) {
 
 $CONFIGURATION = $args[0];
 
-$NUNIT_EXE = Get-ChildItem "packages\NUnit.Runners.**\tools\nunit-console.exe"
+$NUNIT_EXE = Get-ChildItem ".\packages\NUnit.Runners.**\tools\nunit-console.exe"
+$OPENCOVER_EXE = Get-ChildItem ".\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe"
 
 $SEARCH_PATTERN = $NULL
 $TESTS_ASSEMBLIES = $NULL
@@ -22,11 +23,12 @@ else{
 }
 
 if($SEARCH_PATTERN -ne $NULL){
-    $TESTS_ASSEMBLIES = Get-ChildItem $SEARCH_PATTERN
+    $TESTS_ASSEMBLIES = (Get-ChildItem $SEARCH_PATTERN) -join ' '
 }
 
 if($TESTS_ASSEMBLIES -ne $NULL){
-    ForEach($TEST in $TESTS_ASSEMBLIES){
-        Invoke-Expression "$NUNIT_EXE /framework:net-4.0 /result:testresults.xml $TEST"
-    }
+        #Invoke-Expression "$NUNIT_EXE /framework:net-4.0 /result:testresults.xml $TEST"
+		#Invoke-Expression $OPENCOVER_EXE -register:user -target:$NUNIT_EXE -targetargs:$TESTS_ASSEMBLIES -noshadow -appveyor "-filter:+[GitCITestRepo*]*" -excludebyattribute:"*.ExcludeFromCodeCoverage*" -hideskipped:All -output:".\coverage.xml"
+		.\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe -register:user "-filter:+[GitCITestRepo]* -[*Test]*" "-target:$NUNIT_EXE" "-targetargs:/framework:net-4.0 /result:testresults.xml /noshadow $TESTS_ASSEMBLIES" -output:".\coverage.xml"
+		.\packages\ReportGenerator.2.3.2.0\tools\ReportGenerator.exe "-reports:coverage.xml" "-targetdir:.\.coverage"
 }
